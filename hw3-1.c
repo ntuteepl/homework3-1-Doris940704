788595732c08805c27373b1a146fe7bf1d60d059
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-21 16:27:38 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+int main()//作業一
+{
+int i;
+bool n;
+int k=2;
+scanf("%d",&i);
+while((i-1>=k)&&(n=true)){
+    if(i%k==0){
+        n=0;
+        break;
+    }
+    k++;
+}
+if(n==0)
+    printf("No");
+else
+    printf("yes");
+}
